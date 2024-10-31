3483ae29ba21b653999ef96e71ec5af616a84421
JulianM0316 <juliandavidml@gmail.com>
2024-10-30 19:07:21 -0500

    Task 15
    Codigo inconcluso para el parametro en usuarios-clase.php

--- usuarios-clase.php
+++ usuarios-clase.php
@@ -10,7 +10,7 @@ class Usuario {
 
     public function obtenerUsuarioPorId($id) {
         $sql = "SELECT *
-        FROM usuarios
+        FROM usuario
         JOIN cursos ON usuarios.idCurso = cursos.IdCurso
         WHERE usuarios.IdUsuario = ?;
         ";
@@ -27,4 +27,39 @@ class Usuario {
         }
     }
 }
+// Segundo Parametro
+
+public function buscarUsuario($texto,$idCurso) {
+    $sql = '';
+    $stmt = null;
+    $todosLosCursos = -1;
+    if ($texto != null && $idCurso != $todosLosCursos){
+        $sql= "SELECT * FROM usuarios WHERE nombre like ? or apellido like ? or email like ? AND idCurso = ?";
+        $texto = strtolower($texto);
+        $stmt = $this->conex->prepare($sql);
+        $stmt->bind_param("ssss", $texto);
+    }else if ($texto != null && $idCurso == $todosLosCursos){
+        $sql = "SELECT * FROM usuarios WHERE nombre like ? or apellido like ? or email like ?" 
+    }else if ($texto == null){
+         if ($idCurso == $todosLosCursos){
+            $sql = "SELECT * FROM usuarios"
+         }else{
+            $sql = "SELECT * FROM usuarios WHERE idCurso = ?"
+         }
+    }
+    
+
+
+   
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0) {
+        $usuario = $result->fetch_assoc();
+        return $usuario;
+    } else {
+        return null;
+    }
+}
+
 ?>
